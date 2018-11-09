b548390b7ce496a546966f2d9ab4fa1b8b356e63
Anthony Graniero <ananfro@gmail.com>
2018-11-09 02:38:32 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
-  # code goes here
+  #attr_accessor :name
+  @@cats = []
+  
+  def name 
+    @name  
+  end 
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def initialize(name)
+    @name = name
+    @@cats << name
+    cat.name = self
+  end 
+  
+  
+  
 end
